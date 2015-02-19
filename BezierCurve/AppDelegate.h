bcee9167032b25515b1ae649e64838279e773769
PGMY <pgmyjp@gmail.com>
2015-02-19 16:08:43 +0900

    Initial Commit

---
+++ BezierCurve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BezierCurve
+//
+//  Created by Mika Yamamoto on 2015/02/19.
+//  Copyright (c) 2015年 PGMY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BezierCurve/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BezierCurve
+//
+//  Created by Mika Yamamoto on 2015/02/19.
+//  Copyright (c) 2015年 PGMY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
